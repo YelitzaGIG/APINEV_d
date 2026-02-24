02102780f25838e38186c619749a558dd6c54b97
Yelitza Isidro <79675637+YelitzaGIG@users.noreply.github.com>
2026-02-24 11:20:44 -0600

    Delete views directory

--- views/_partials/footer.php
+++
@@ -1,8 +0,0 @@
-<?php // views/_partials/footer.php ?>
-<footer class="site-footer">
-  <div class="footer-logo">ITS<br>OEH</div>
-  <p>contacto del <?= APP_INST ?></p>
-  <div class="footer-logo">ITS<br>OEH</div>
-</footer>
-</body>
-</html>
